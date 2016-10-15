3e8fe6b9fff6b325ded926908d21f5e915c9869a
floodpeak <hongfengzhou@yahoo.com>
2016-10-15 23:22:52 +0800

    ut, for 3, failed

--- test/getPrimeFactors.test.js
+++ test/getPrimeFactors.test.js
@@ -8,4 +8,7 @@ describe('prime factor', ()=>{
   it('should be [2] for 2', ()=>{
     expect(getPrimeFactors(2)).to.be.eql([2])
   })
+  it('should be [3] for 3', ()=>{
+    expect(getPrimeFactors(3)).to.be.eql([3])
+  })
 })

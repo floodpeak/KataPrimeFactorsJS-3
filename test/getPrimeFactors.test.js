6dd2c56ec320edda39f40db5e0c4ba7ca0ec3f1b
floodpeak <hongfengzhou@yahoo.com>
2016-10-15 23:20:46 +0800

    ut, correct the it description

--- test/getPrimeFactors.test.js
+++ test/getPrimeFactors.test.js
@@ -2,7 +2,7 @@ import { expect } from 'chai'
 import getPrimeFactors from '../getPrimeFactors'
 
 describe('prime factor', ()=>{
-  it('should be 1 for 1', ()=>{
+  it('should be [] for 1', ()=>{
     expect(getPrimeFactors(1)).to.be.eql([])
   })
 })

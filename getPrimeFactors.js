80650351a1c348e0339c7836e93c6594352ad74d
floodpeak <hongfengzhou@yahoo.com>
2016-10-15 23:40:17 +0800

    refactor, make the logic for 6 looks the same to 4

--- getPrimeFactors.js
+++ getPrimeFactors.js
@@ -1,8 +1,9 @@
 export default (number)=>{
-  if(number === 6){
-    return [2,3]
-  }
   const result = []
+  if(number === 6){
+    result.push(2)
+    number /= 2
+  }
   if(number === 4){
     result.push(2)
     number /= 2

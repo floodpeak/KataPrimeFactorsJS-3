796a61a4039022d812d28381ab9bbb9aefb9d520
floodpeak <hongfengzhou@yahoo.com>
2016-10-15 23:47:00 +0800

    refactor, move achievement for 9 diving into codes

--- getPrimeFactors.js
+++ getPrimeFactors.js
@@ -1,12 +1,12 @@
 export default (number)=>{
-  if(number === 9){
-    return [3,3]
-  }
   const result = []
   const base = 2
   if(number % base === 0){
     result.push(base)
     number /= base
+  }
+  if(number === 9){
+    return [3,3]
   }
   if(number >1){
     result.push(number)

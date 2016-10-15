78490b9aacea0f27b56314fd6125f5095cf59868
floodpeak <hongfengzhou@yahoo.com>
2016-10-16 00:04:47 +0800

    refactor, change condition for first loop

--- getPrimeFactors.js
+++ getPrimeFactors.js
@@ -1,13 +1,13 @@
 export default (number)=>{
   const result = []
 
-  for(let base = 2;base < number;base++){
+  for(let base = 2;base <= number;base++){
     for(;number % base === 0;number /= base){
       result.push(base)
     }
   }
-  if(number >1){
-    result.push(number)
-  }
+  // if(number >1){
+  //   result.push(number)
+  // }
   return result
 }

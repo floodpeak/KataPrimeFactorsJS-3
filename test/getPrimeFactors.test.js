930f83b1c15058ab1329679bd842ec468e75d94c
floodpeak <hongfengzhou@yahoo.com>
2016-10-15 23:21:32 +0800

    ut, for 2, failed

--- test/getPrimeFactors.test.js
+++ test/getPrimeFactors.test.js
@@ -5,4 +5,7 @@ describe('prime factor', ()=>{
   it('should be [] for 1', ()=>{
     expect(getPrimeFactors(1)).to.be.eql([])
   })
+  it('should be [2] for 2', ()=>{
+    expect(getPrimeFactors(2)).to.be.eql([2])
+  })
 })

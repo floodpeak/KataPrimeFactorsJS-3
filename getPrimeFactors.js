ae430823c023928757f833532118f79cacdddff7
floodpeak <hongfengzhou@yahoo.com>
2016-10-15 23:48:43 +0800

    refactor, pull magic number 3 out

--- getPrimeFactors.js
+++ getPrimeFactors.js
@@ -1,13 +1,14 @@
 export default (number)=>{
   const result = []
-  const base = 2
+  let base = 2
   if(number % base === 0){
     result.push(base)
     number /= base
   }
+  base = 3
   if(number === 9){
-    result.push(3)
-    number /= 3
+    result.push(base)
+    number /= base
   }
   if(number >1){
     result.push(number)

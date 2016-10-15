3d50abf7dcfad168a2051da595550a3f41478462
floodpeak <hongfengzhou@yahoo.com>
2016-10-16 00:03:54 +0800

    refactor, change while loop into for loop

--- getPrimeFactors.js
+++ getPrimeFactors.js
@@ -2,9 +2,8 @@ export default (number)=>{
   const result = []
 
   for(let base = 2;base < number;base++){
-    while(number % base === 0){
+    for(;number % base === 0;number /= base){
       result.push(base)
-      number /= base
     }
   }
   if(number >1){
